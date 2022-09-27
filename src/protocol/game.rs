fe3b604bc0862a68cf145de9139cafcc73280fab
Bryan McClain <bamcclain10@gmail.com>
2022-09-27 09:25:15 -0400

    Updated the GameState object

--- src/protocol/game.rs
+++ src/protocol/game.rs
@@ -35,10 +35,23 @@ pub enum GameStateUpdate {
   },
 }
 
+/// Get the current game state
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct GameState {
+  /// Static obstacles in the arena (like walls)
+  #[serde(default)]
   playfield: Vec<Vec<u32>>,
+
+  /// Map of player ID to details
+  #[serde(default)]
   players: HashMap<Uuid, serde_json::Map<String, serde_json::Value>>,
+
+  /// List of weapons in the arena
+  #[serde(default)]
   weapons: Vec<serde_json::Map<String, serde_json::Value>>,
+
+  /// List of items in the arena
+  #[serde(default)]
+  items: Vec<serde_json::Map<String, serde_json::Value>>,
 }
